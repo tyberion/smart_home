8bbf1cf2378b7da8cec33bb2661f0442e5910fb2
Maarten Kruithof <mckruithuijb@gmail.com>
2018-06-02 07:13:28 +0000

    Adds more files and a working app

---
+++ app.py
@@ -0,0 +1,73 @@
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+
+from flask import Flask, render_template, request
+import logging
+from logging import Formatter, FileHandler
+import os
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+app.config.from_object('config')
+#db = SQLAlchemy(app)
+
+# Automatically tear down SQLAlchemy.
+'''
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
+'''
+
+# Login required decorator.
+'''
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+'''
+#----------------------------------------------------------------------------#
+# Controllers.
+#----------------------------------------------------------------------------#
+
+
+@app.route('/')
+def home():
+    return render_template('layout.html')
+
+
+# Error handlers.
+
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+#----------------------------------------------------------------------------#
+# Launch.
+#----------------------------------------------------------------------------#
+
+# Default port:
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
