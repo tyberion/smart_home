bef82fdc756900be88db1085e5a502976345565d
Maarten Kruithof <mckruithuijb@gmail.com>
2018-06-05 21:17:51 +0200

    Adds Netatmo data ingestion

---
+++ fetch_netatmo.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from datetime import datetime
+
+import netatmo
+from credentials import NETATMO_ID, NETATMO_PASSWORD, NETATMO_SECRET, NETATMO_STATION_ID, NETATMO_USERNAME
+
+
+def netatmo_data():
+    # credentials as parameters
+    ws = netatmo.WeatherStation(
+        {'client_id': NETATMO_ID,
+         'client_secret': NETATMO_SECRET,
+         'username': NETATMO_USERNAME,
+         'password': NETATMO_PASSWORD,
+         'default_station': NETATMO_STATION_ID,
+         })
+    ws.get_data()
+    module = ws.devices[0]
+    module_data = [module['dashboard_data']] + [m['dashboard_data'] for m in module['modules']]
+    data = pd.DataFrame(module_data)
+    data['datetime'] = data.time_utc.apply(datetime.fromtimestamp)
+    # data['module_name'] = [module['module)name']0
+    data['module_name'] = [module['module_name']] + [m['module_name'] for m in module['modules']]
+
+    return data[['datetime', 'module_name', 'CO2', 'Humidity', 'Noise', 'Pressure', 'Temperature']]
+
+
+if __name__ == '__main__':
+    mdata, m = netatmo_data()
