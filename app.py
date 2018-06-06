af41715faaba8f4994d84fe1aa17690986944e26
Maarten Kruithof <mckruithuijb@gmail.com>
2018-06-06 16:00:03 +0200

    Adds graph for various values

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ import logging
 import json
 from logging import FileHandler, Formatter
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from sheets import ROW_TITLES
 
 import plotly
 import plotly.graph_objs as go
 
 from manage_data import read_data
+from datetime import datetime, timedelta
+from werkzeug.contrib.cache import SimpleCache
+
+cache = SimpleCache()
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -20,7 +25,23 @@ def home():
 
 @app.route('/graph')
 def graph():
-    data = read_data()
+    graph_date = request.args.get('date')
+    graph_type = request.args.get('type')
+    if graph_date is None:
+        graph_date = datetime.now().strftime('%Y%m%d')
+    if graph_type not in ROW_TITLES:
+        graph_type = 'Temperature'
+    date_range = [datetime.strptime(graph_date, '%Y%m%d')] * 2
+    date_range[0] -= timedelta(0)
+    date_range[1] += timedelta(1)
+    date_range = [datetime.strftime(dt, '%Y-%m-%d') for dt in date_range]
+
+    data = cache.get('data')
+    if data is None:
+        data = read_data()
+        cache.set('data', data, timeout=10 * 60)
+
+    data = data[data.Datetime.dt.strftime('%Y%m%d') == graph_date]
 
     # Create a trace
     traces = []
@@ -28,32 +49,22 @@ def graph():
         trace = go.Scatter(
             name=g,
             x=d.Datetime,
-            y=d.Temperature,
+            y=d[graph_type],
         )
-        traces.append(trace)
+        if not d[graph_type].isnull().values.all():
+            traces.append(trace)
 
     layout = go.Layout(
         title='Sensor Measurements',
         yaxis=dict(
-            title='Temperature',
-            range=[0, 40],
+            title=graph_type,
+            # range=[0, 40],
         ),
         xaxis=dict(
+            range=date_range,
         ),
         showlegend=True)
     graph = go.Figure(data=traces, layout=layout)
-    # graph = dict(
-    #     data=traces,
-    #     layout=dict(
-    #         title='Sensor Measurements',
-    #         yaxis=dict(
-    #             title='Temperature'
-    #         ),
-    #         xaxis=dict(
-    #             title='Time'
-    #         )
-    #     )
-    # )
     graphJSON = json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder)
     return render_template('graph.html', graphJSON=graphJSON)
 
